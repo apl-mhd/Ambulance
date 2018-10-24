0ae816ba1ee7774f4addc71536393e92bc9aa7b2
apl-mhd <mmahmud161036@bscse.uiu.ac.bd>
2018-10-24 14:55:02 +0600

    Update facker mobile number

--- database/factories/AmbulanceInfoFactory.php
+++ database/factories/AmbulanceInfoFactory.php
@@ -9,14 +9,15 @@ $factory->define(App\Models\AmbulanceInfo::class,function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         'drivername' => $faker->name,
         'ownername' => $faker->name,
-        'drivermobile' => '0171020020',
-        'ownermobile' => '0171010101',
-        'drivernid' => '1995102434343',
+        'drivermobile' =>'017'.rand(0,9).rand(0,9).mt_rand(000000,999999),
+        'ownermobile' => '017'.rand(0,9).rand(0,9).mt_rand(000000,999999),
+        'drivernid' => mt_rand(19680000000000,19689999999999),
         'ambulancename' => $faker->name,
         'location' => 'Dhaka',
         'hospital' => 'Ibnasina',
         'acstatus' =>random_int(0,1),
         'numberplate' => 'Dhaka L - 1234',
         'type' =>'ICU',
+        'imglink' => $faker->imageUrl(),
     ];
 });
